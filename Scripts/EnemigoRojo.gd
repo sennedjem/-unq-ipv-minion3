f3269750637494e2eee3763b9c20430067bf2758
salopez <sebastian.lopez@trinom.io>
2020-11-14 21:31:36 -0300

    Se agrega comportamiento de que si el pj salta sobre la cabeza de los enemigos mueren

--- Scripts/EnemigoRojo.gd
+++ Scripts/EnemigoRojo.gd
@@ -8,7 +8,7 @@ export (int) var run_speed = 60
 export (int) var jump_speed = -340
 export (int) var gravity = 1200
 var rng = RandomNumberGenerator.new()
-
+var dead = false
 var active = false
 var waiting = false
 
@@ -20,11 +20,9 @@ func _ready():
 	rng.randomize()
 	var random = rng.randf()
 	if random>0.5:
-		print('mas')
 		direcction = "right"
 		$AnimatedSprite.flip_h = false
 	else: 
-		print('menos')
 		direcction ="left"	
 		$AnimatedSprite.flip_h = true
 	pass # Replace with function body.
@@ -32,32 +30,33 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	velocity.x = 0
-	if(direcction=="right"):
-		if(self.position.x>_get_top_right_()):
-			waiting = true
-			if($Timer.is_stopped()):
-				var secs = rng.randf()
-				print(secs)
-				$Timer.start(secs)
-		velocity.x += run_speed
-	else:
-		if(self.position.x<-33):
-			waiting = true
-			if($Timer.is_stopped()):
-				var secs = rng.randf()
-				print(secs)
-				$Timer.start(secs)
-		velocity.x -= run_speed	
-	velocity.y += gravity * delta
-	if active:
-		if(!waiting):
-			velocity = move_and_slide(velocity,Vector2(0, -1))
-			$AnimatedSprite.play("correr")
+	if!dead:
+		velocity.x = 0
+		if(direcction=="right"):
+			if(self.position.x>_get_top_right_()):
+				waiting = true
+				if($Timer.is_stopped()):
+					var secs = rng.randf()
+					print(secs)
+					$Timer.start(secs)
+			velocity.x += run_speed
 		else:
-			$AnimatedSprite.play("ocioso")
-	else:
-		$AnimatedSprite.play("estatico")
+			if(self.position.x<-33):
+				waiting = true
+				if($Timer.is_stopped()):
+					var secs = rng.randf()
+					print(secs)
+					$Timer.start(secs)
+			velocity.x -= run_speed	
+		velocity.y += gravity * delta
+		if active:
+			if(!waiting):
+				velocity = move_and_slide(velocity,Vector2(0, -1))
+				$AnimatedSprite.play("correr")
+			else:
+				$AnimatedSprite.play("ocioso")
+		else:
+			$AnimatedSprite.play("estatico")
 
 
 func _on_Level_juego_activo():
@@ -82,3 +81,17 @@ func _on_Timer_timeout():
 	else:
 		direcction = "right"
 		$AnimatedSprite.flip_h = false	
+
+
+func _on_Area2D_body_entered(body):
+	print(body.name)
+	if(body.name == "PersonajeJugable"):
+		dead = true
+		$AnimatedSprite.play("muerte")
+
+
+func _on_AnimatedSprite_animation_finished():
+	if($AnimatedSprite.animation =="muerte"):
+		$AnimatedSprite.stop()
+		$AnimatedSprite.set_animation("muerte")
+		$AnimatedSprite.set_frame(6)
