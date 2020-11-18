b5ab3a0c1187711dde63122b516b30817bd8b808
Rodrigo Papadopulo <rpapadopulo.2106@gmail.com>
2020-11-17 23:05:16 -0300

    Plataforma movible y enemigos ultimo nivel

--- Scripts/TercerNivel.gd
+++ Scripts/TercerNivel.gd
@@ -8,11 +8,35 @@ var esta_en_final = false
 signal juego_frenado()
 signal juego_activo()
 
+var enemigos = {}
+
 func _ready():
 	$"Trampa/Caja Activa/AnimatedSprite".play("fin")
 	$PersonajeJugable/Camera2D.limit_right = 1436
 	$MusicaFondo.play()
 	$"Top Line/Time".text = str(seconds)
+	
+	enemigos = {
+			"EnemigoRojo1" : $"Plataformas flotantes/Plataformita/EnemigoRojo1",
+			"EnemigoRojo2" : $"Plataformas flotantes/Plataformita3/EnemigoRojo2",
+			"EnemigoRojo3" : $Piso/EnemigoRojo3,
+			"EnemigoRojo4" : $"Plataformas flotantes/Plataformita2/EnemigoRojo4",
+			"EnemigoRojo5" : $Piso/EnemigoRojo5,
+			"EnemigoRojo6" : $"Plataformas flotantes/Plataformita4/EnemigoRojo6"
+		}
+	
+	$"Plataformas flotantes/Plataformita/EnemigoRojo1".left_top_distance = -40
+	$"Plataformas flotantes/Plataformita/EnemigoRojo1".right_top_distance = 43
+	$"Plataformas flotantes/Plataformita2/EnemigoRojo4".right_top_distance = 42
+	$"Plataformas flotantes/Plataformita2/EnemigoRojo4".left_top_distance = -44
+	$"Plataformas flotantes/Plataformita3/EnemigoRojo2".right_top_distance = 40
+	$"Plataformas flotantes/Plataformita3/EnemigoRojo2".left_top_distance = -40
+	$"Plataformas flotantes/Plataformita4/EnemigoRojo6".right_top_distance = 50
+	$"Plataformas flotantes/Plataformita4/EnemigoRojo6".left_top_distance = -40
+	$Piso/EnemigoRojo3.left_top_distance = 405
+	$Piso/EnemigoRojo3.right_top_distance = 555
+	$Piso/EnemigoRojo5.left_top_distance = 612
+	$Piso/EnemigoRojo5.right_top_distance = 834
 	
 func _frenar_juego():
 	emit_signal("juego_frenado")
@@ -30,6 +54,7 @@ func _retomar_juego():
 		$Timer.start()
 	
 func _process(delta):
+	set_position_to_enemies()
 	if (coins == 10):
 		puede_avanzar = true
 		$CajaFinal/AnimatedSprite.play("fin")
@@ -74,7 +99,6 @@ func _on_CajaFinal_personaje_salio():
 func check_ending():
 	if(puede_avanzar):
 		if(esta_en_final && Input.is_action_just_pressed('ui_accept')):
-			print("Hola")
 			get_tree().change_scene("res://Resources/JuegoFinalizado.tscn")
 
 func _on_Caja_Activa_personaje_entro():
@@ -85,3 +109,11 @@ func _on_Caja_Activa_personaje_salio():
 
 func _on_MusicaFondo_finished():
 	$MusicaFondo.play()
+
+
+func _on_enemigo_murio(body):
+	enemigos.erase(body.name)
+	
+func set_position_to_enemies():
+	for enemigo in enemigos.values():
+		enemigo.personajeJugablePosition = $PersonajeJugable.global_position
